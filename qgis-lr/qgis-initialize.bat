3849f1cbe88e6c9e56685e3a02f8823c9a095cde
bscholle <45168872+bscholle@users.noreply.github.com>
2018-11-19 13:44:13 +0100

    Add files via upload

---
+++ qgis-lr/qgis-initialize.bat
@@ -0,0 +1,37 @@
+REM Batch-Datei for initialize QGIS 3
+echo on
+chcp 1252
+
+REM Text for the desktop shortcut.
+set "QGIS_TEXT=QGIS 3 (serverbasiert, OSGeo4W)"
+set "OSGEO4W_ROOT=Q:\OSGeo4W64"
+
+REM Create shortcut in the programs group for all user (public)
+if not exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\QGIS\%QGIS_TEXT%.lnk" (
+   nircmd shortcut """%OSGEO4W_ROOT%""\bin\qgis-start.bat" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\QGIS" "%QGIS_TEXT%" "" """%OSGEO4W_ROOT%""\icons\QGIS.ico" "0" "min" """%OSGEO4W_ROOT%""\bin" ""
+   )
+
+if not exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\QGIS\OSGeo4W Shell.lnk" (
+   nircmd shortcut """%OSGEO4W_ROOT%""\OSGeo4W.bat" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\QGIS" "OSGeo4W Shell" "" """%OSGEO4W_ROOT%""\OSGeo4W.ico" "" "" """\" ""
+   )
+
+REM Create shortcut on desktop for all user (public)
+if not exist "%PUBLIC%\Desktop\%QGIS_TEXT%.lnk" (
+   nircmd shortcut """%OSGEO4W_ROOT%""\bin\qgis-start.bat" "%PUBLIC%\Desktop" "%QGIS_TEXT%" "" """%OSGEO4W_ROOT%""\icons\QGIS.ico" "0" "min" """%OSGEO4W_ROOT%""\bin" ""
+   )
+
+REM Create file and icon associations 
+start/wait regedit -s "%OSGEO4W_ROOT%\bin\qgis.reg"
+
+assoc .qgs=QGIS Project
+assoc .qlr=QGIS Layer Definition
+assoc .qml=QGIS Layer Settings
+assoc .qpt=QGIS Composer Template
+
+REM Install Symbol-Fonts 
+REM copy /Y \\<Netzwerkpfad zur Font-Datei>\<Name der Fontdatei>.ttf C:\Windows\Fonts\<Name der Fontdatei>.ttf
+
+REM Registry Symbol-Fonts
+REM start /wait regedit -s "%OSGEO4W_ROOT%\bin\<Name der Reg-Datei>.reg"
+
+exit /b
---
+++ qgis-lr/qgis-start-prepare.bat
@@ -0,0 +1,29 @@
+@echo off
+call "%~dp0\o4w_env.bat"
+call qt5_env.bat
+call py3_env.bat
+@echo off
+path %OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
+set GDAL_FILENAME_IS_UTF8=YES
+rem Set VSI cache to be used as buffer, see #6448
+set VSI_CACHE=TRUE
+set VSI_CACHE_SIZE=1000000
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
+
+REM start "QGIS" /B "%OSGEO4W_ROOT%\bin\qgis-bin.exe" %*
+
+
+REM Version wird aus Pfad ausgelesen ("P:\qgis\v3\..." -> "v3")
+REM set "QGIS_VER=%OSGEO4W_ROOT%
+REM for %%f in (%QGIS_VER%) do set QGIS_VER=%%~nxf
+
+set "QGIS_UDIR=%USERPROFILE%\AppData\Roaming\QGIS\OSGeo4W\qgis-lr"
+
+REM Modus setzen (RUN|) [leer lassen für Konfigurationsmodus; "RUN" für Nutzerbetrieb]
+set "QGIS_MODE="
+call "%OSGEO4W_ROOT%\bin\qgis-prepare.bat" %QGIS_MODE%
+
+REM start "QGIS" /B "%OSGEO4W_ROOT%\bin\qgis-bin.exe" --profiles-path "%QGIS_UDIR%" --profile default %*
+start "QGIS" /B "%OSGEO4W_ROOT%\bin\qgis-bin.exe" --profiles-path "%QGIS_UDIR%" %*
+
