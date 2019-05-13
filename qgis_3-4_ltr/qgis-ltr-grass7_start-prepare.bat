e1166a87c3c87035b2474eaf43cb6db7ac928d42
Thomas Schüttenberg <thomas@qgis.de>
2019-05-13 16:04:41 +0200

    Update and rename qgis-ltr-grass7-start-prepare.bat to qgis-ltr-grass7_start-prepare.bat

--- qgis_3-4_ltr/qgis-ltr-grass7_start-prepare.bat
+++ qgis_3-4_ltr/qgis-ltr-grass7_start-prepare.bat
@@ -18,6 +18,6 @@ set "QGIS_UDIR=%USERPROFILE%\AppData\Roaming\QGIS\OSGeo4W\qgis-ltr"
 
 REM Modus setzen (RUN|) [leer lassen für Konfigurationsmodus; "RUN" für Nutzerbetrieb]
 set "QGIS_MODE="
-call "%OSGEO4W_ROOT%\bin\qgis-ltr-grass7-prepare.bat" %QGIS_MODE%
+call "%OSGEO4W_ROOT%\bin\qgis-ltr-grass7_prepare.bat" %QGIS_MODE%
 
 start "QGIS" /B "%OSGEO4W_ROOT%\bin\qgis-ltr-bin-g7.exe" --profiles-path "%QGIS_UDIR%" %*
